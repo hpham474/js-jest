a242fec13145d42db63998a916684c913a421cb3
hpham474 <hpham474@gmail.com>
2024-08-02 04:02:06 -0500

    pass capitalize tests

--- src/functions/capitalize.js
+++ src/functions/capitalize.js
@@ -1,3 +1,7 @@
-function capitalize() {}
+function capitalize(string) {
+  const firstChar = string.charAt(0);
+
+  return firstChar.toUpperCase() + string.slice(1);
+}
 
 export default capitalize;
