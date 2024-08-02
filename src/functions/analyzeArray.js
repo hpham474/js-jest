62035a18886c169dee729fa41937f0b34c11167c
hpham474 <hpham474@gmail.com>
2024-08-02 05:24:40 -0500

    pass analyzeArray test

--- src/functions/analyzeArray.js
+++ src/functions/analyzeArray.js
@@ -1,3 +1,22 @@
-function analyzeArray() {}
+function analyzeArray(array) {
+  let min = array[0];
+  let max = array[0];
+  let sum = 0;
+  const length = array.length;
+
+  for (const num of array) {
+    if (min > num) {
+      min = num;
+    }
+    if (max < num) {
+      max = num;
+    }
+    sum = sum + num;
+  }
+
+  const avg = sum / length;
+
+  return { average: avg, min: min, max: max, length: length };
+}
 
 export default analyzeArray;
